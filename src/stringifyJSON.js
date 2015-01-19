39970fe8e0fc3dece616f92dc5f5468b0161a5b3
Conor Flannigan <cflanni1@jhu.edu>
2015-01-19 10:34:20 -0800

    Implement stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,41 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  if (typeof obj === "number" ||
+      typeof obj === "boolean") {
+    return obj.toString();
+  } else if (typeof obj === "string") {
+    return '"' + obj + '"';
+  } else if (obj === null) {
+    return 'null';
+  }
+  else if (typeof obj === "function" ||
+           typeof obj === "symbol" ||
+           typeof obj === "undefined") {
+    return '';
+  }
+  else if (Array.isArray(obj)) {
+    return '[' + obj.map( function(el) {
+      return stringifyJSON(el);
+    }) + ']';
+  }
+  else {
+    var string = "{";
+    Object.keys(obj).forEach( function(key, index, keys) {
+      console.log('type of key: ', typeof key);
+      if (typeof obj[key] === "function" ||
+          typeof obj[key] === "symbol" ||
+          typeof obj[key] === "undefined") {
+        // do nothing
+      } else {
+        console.log(key, obj[key]);
+        string += '"' + key + '":' + stringifyJSON(obj[key]);
+        if (index < keys.length-1) {
+          string += ',';
+        }
+      }
+    });
+    string += "}";
+    return string;
+  }
 };
