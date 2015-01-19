4f08b6ca55b502b14cc1b0e8b05b48cb6791ca76
Conor Flannigan <cflanni1@jhu.edu>
2015-01-19 12:35:24 -0800

    Implement getElementsByClassName

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,5 +6,36 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className
 ){
-  // your code here
+
+  var result = [];
+
+  /* Recursive helper function.
+   * Pushes elements to result array if they have
+   * matching class names 
+   */
+  function getEls(node, classes) {
+    if (node.nodeType == document.ELEMENT_NODE) {
+      
+      var match = true;
+      for (var i = 0; i < classes.length; i++) {
+        if (node.classList.contains(classes[i]) === false)
+          match = false;
+      }
+      if (match)
+        result.push(node);
+      
+      // recur on child nodes
+      for (var j = 0; j < node.childNodes.length; j++)
+        getEls(node.childNodes[j], classes);
+      
+    } else if (node.nodeType == document.TEXT_NODE) {
+      // do nothing
+    }
+  }
+
+  var classes = className.split(/\s+/);
+  getEls(document.body, classes);
+
+  return result;
+  
 };
